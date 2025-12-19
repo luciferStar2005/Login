2ce7abcb10fe6e47d1b74a38fb480a6233330484
lucifer <ortizcalet2@gmail.com>
2025-12-18 19:59:48 -0500

    primeraparte

---
+++ src/test/java/org/caletortiz/authlogin/AuthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.caletortiz.authlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
